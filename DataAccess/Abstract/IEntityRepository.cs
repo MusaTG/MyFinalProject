12d6b44ffdb4c38c12f750f07832e91a076af7fe
MusaTG <49027797+MusaTG@users.noreply.github.com>
2021-10-21 17:09:57 +0300

    Core katmanı eklendi. category gerekli class eklendi. birkaç veri erişimi sağlandı.

--- DataAccess/Abstract/IEntityRepository.cs
+++ DataAccess/Abstract/IEntityRepository.cs
@@ -1,21 +1,6 @@
-﻿using Entities.Abstract;
-using System;
-using System.Collections.Generic;
-using System.Linq.Expressions;
-using System.Text;
-
-namespace DataAccess.Abstract
+﻿namespace DataAccess.Abstract
 {
-    //generic constraint
-    //class : referans tip
-    //IEntity : IEntity olabilir veya IEntity implemente eden bir nesne olabilir
-    //new : new'lenebilir olmalı
-    public interface IEntityRepository<T> where T:class,IEntity
+    public interface IEntityRepository<T1, T2>
     {
-        List<T> GetAll(Expression<Func<T,bool>> filter=null);
-        T Get(Expression<Func<T, bool>> filter);
-        void Add(T entity);
-        void Update(T entity);
-        void Delete(T entity);
     }
 }
