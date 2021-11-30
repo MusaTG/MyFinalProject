d79806dfa196f4b8555fe000b7fb1d376aabe59a
MusaTG <49027797+MusaTG@users.noreply.github.com>
2021-11-30 14:48:10 +0300

    Bazı iş kuralları eklendi.(kategorideki ürün sınırı, aynı ürün eklenmemesi, kategorinin ürün sayısı kontrolü sağlanması)

--- Business/Concrete/ProductManager.cs
+++ Business/Concrete/ProductManager.cs
@@ -1,8 +1,10 @@
 ﻿using Business.Abstract;
+using Business.CCS;
 using Business.Constants;
 using Business.ValidationRules.FluentValidation;
 using Core.Aspects.Autofac.Validation;
 using Core.CrossCuttingConcerns.Validation;
+using Core.Utilities.Business;
 using Core.Utilities.Results;
 using DataAccess.Abstract;
 using Entities.Concrete;
@@ -10,6 +12,7 @@ using Entities.DTOs;
 using FluentValidation;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 
 namespace Business.Concrete
@@ -17,33 +20,39 @@ namespace Business.Concrete
     public class ProductManager : IProductService
     {
         IProductDal _productDal;
+        ICategoryService _categoryService;
 
-        public ProductManager(IProductDal productDal)
+        public ProductManager(IProductDal productDal, ICategoryService categoryService)
         {
             _productDal = productDal;
+            _categoryService = categoryService;
         }
 
         [ValidationAspect(typeof(ProductValidator))]
         public IResult Add(Product product)
         {
-            //business code
             //validation
+            {
+                //--------ProductValidation içine geçildi-------
+                //if(product.UnitPrice<=0)
+                //{
+                //    return new ErrorResult(Messages.UnitPriceInvalid);
+                //}
 
+                //if (product.ProductName.Length < 2)
+                //{
+                //    //magic strings
+                //    return new ErrorResult(Messages.ProductNameInvalid);
+                //}
 
-            //--------ProductValidation içine geçildi-------
-            //if(product.UnitPrice<=0)
-            //{
-            //    return new ErrorResult(Messages.UnitPriceInvalid);
-            //}
-
-            //if (product.ProductName.Length < 2)
-            //{
-            //    //magic strings
-            //    return new ErrorResult(Messages.ProductNameInvalid);
-            //}
-
-            //ValidationTool.Validate(new ProductValidator(), product);//Bu kod satırı yukarıdaki validationascept içinde geçiyor
-
+                //ValidationTool.Validate(new ProductValidator(), product);//Bu kod satırı yukarıdaki validationascept içinde geçiyor
+            }
+            IResult result = BusinessRules.Run(CheckIfProductNameExists(product.ProductName),
+                CheckIfProductCountOfCategoryCorrect(product.CategoryId), CheckIfCategoryLimitExceded());
+            if (result != null)
+            {
+                return result;
+            }
             _productDal.Add(product);
             return new SuccessResult(Messages.ProductAdded);
         }
@@ -57,7 +66,7 @@ namespace Business.Concrete
                 return new ErrorDataResult<List<Product>>(Messages.MaintenanceTime);
             }//sistem bakımı gibi bişey
 
-            return new SuccessDataResult<List<Product>>(_productDal.GetAll(),Messages.ProductsListed);
+            return new SuccessDataResult<List<Product>>(_productDal.GetAll(), Messages.ProductsListed);
         }
 
         public IDataResult<List<Product>> GetAllByCategory(int id)
@@ -82,6 +91,41 @@ namespace Business.Concrete
             //    return new ErrorDataResult<List<ProductDetailDto>>(Messages.MaintenanceTime);
             //}
             return new SuccessDataResult<List<ProductDetailDto>>(_productDal.GetProductDetails());
+        }
+
+        [ValidationAspect(typeof(ProductValidator))]
+        public IResult Update(Product product)
+        {
+            throw new NotImplementedException();
+        }
+
+        private IResult CheckIfProductCountOfCategoryCorrect(int categoryId)
+        {
+            var result = _productDal.GetAll(p => p.CategoryId == categoryId).Count;
+            if (result >= 10)
+            {
+                return new ErrorResult(Messages.ProductCountOfCategoryError);
+            }
+            return new SuccessResult();
+        }
+
+        private IResult CheckIfProductNameExists(string productName)
+        {
+            var result = _productDal.GetAll(p => p.ProductName == productName).Any();
+            if (result)
+            {
+                return new ErrorResult(Messages.ProductNameAlreadyExists);
+            }
+            return new SuccessResult();
+        }
+        private IResult CheckIfCategoryLimitExceded()
+        {
+            var result = _categoryService.GetAll();
+            if (result.Data.Count > 15)
+            {
+                return new ErrorResult(Messages.CategoryLimitExceded);
+            }
+            return new SuccessResult();
         }
     }
 }
--- Business/Constants/Messages.cs
+++ Business/Constants/Messages.cs
@@ -10,5 +10,8 @@ namespace Business.Constants
         public static string ProductNameInvalid = "Ürün ismi geçersiz.";
         public static string ProductsListed = "Ürünler listelendi.";
         public static string MaintenanceTime = "Sistem bakımda...";
+        public static string ProductCountOfCategoryError = "Bir kategoride en fazla 10 olabilir.";
+        public static string ProductNameAlreadyExists = "Bu isimde başka bir ürün var.";
+        public static string CategoryLimitExceded = "Bu kategoride ürün limiti aşıldığı için yeni ürün eklenemiyor.";
     }
 }
